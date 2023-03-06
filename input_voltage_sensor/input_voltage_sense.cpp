7abc3796194e91de2a9431fddf5702cda61d478a
tjsos <tony.jacob@uri.edu>
2023-03-06 15:54:35 -0500

    Modified header files

--- input_voltage_sensor/input_voltage_sense.cpp
+++ input_voltage_sensor/input_voltage_sense.cpp
@@ -10,26 +10,6 @@ Author : Tony Jacob, tony.jacob@uri.edu
 #include "hardware/adc.h"
 #include "input_voltage_sense.h"
 
-class InputV{
-    public:
-
-    InputV(int gpio,int adc){
-        stdio_init_all();
-        adc_init();
-
-        adc_gpio_init(gpio);
-        adc_select_input(adc);
-    }
-
-    void readVoltage(){
-        // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
-        const float conversion_factor = 3.3f / (1<<12);
-        uint16_t result_at_VB = adc_read();
-        // 11 is from the voltage divide formula.
-        printf("Raw value: 0x%03x, voltage: %f V\n", result_at_VB, result_at_VB * conversion_factor * 11);
-    }
-};
-
 int main(){
     InputV input(gpio, adc);
     while(1)
--- input_voltage_sensor/input_voltage_sense.h
+++ input_voltage_sensor/input_voltage_sense.h
@@ -5,3 +5,23 @@ Author : Tony Jacob, tony.jacob@uri.edu
 
 int gpio = 26;
 int adc = 0;
+
+class InputV{
+    public:
+
+    InputV(int gpio,int adc){
+        stdio_init_all();
+        adc_init();
+
+        adc_gpio_init(gpio);
+        adc_select_input(adc);
+    }
+
+    void readVoltage(){
+        // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
+        const float conversion_factor = 3.3f / (1<<12);
+        uint16_t result_at_VB = adc_read();
+        // 11 is from the voltage divide formula.
+        printf("Raw value: 0x%03x, voltage: %f V\n", result_at_VB, result_at_VB * conversion_factor * 11);
+    }
+};
